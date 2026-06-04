a22aa9ad747484997d3475341706212cd04e4d3f
sj.concentrix <61546301+dope0515@users.noreply.github.com>
2026-06-04 14:53:42 +0900

    ADD : filter-wrap 내 위치 기반 검색 추가

--- pages/index.vue
+++ pages/index.vue
@@ -2,27 +2,29 @@
   <section class="main-section">
     <div class="inner">
       <div class="content-wrap">
-        <div class="title-bx">
-          <h1 class="title">잘하는 집을<br><span>안 가봐서 </span>그래</h1>
-        </div>
-        <div class="desc-bx">
-          <p>그 음식이 별로라고?<br>그건 자네가 진짜 잘하는 집을 안 가봐서 그래</p>
-          <div class="btn-bx">
-            <AppButton
-              to="/restaurants"
-              color="black"
-              shape="round"
-              arrow>
-              잘하는 집 보러가기
-            </AppButton>
-            <AppButton
-              color="black"
-              variant="outline"
-              shape="round"
-              arrow
-              @click="goToRegister">
-              잘하는 집 알려주기
-            </AppButton>
+        <div class="text-wrap">
+          <div class="title-bx">
+            <h1 class="title">잘하는 집을<br><span>안 가봐서 </span>그래</h1>
+          </div>
+          <div class="desc-bx">
+            <p>그 음식이 별로라고?<br>그건 자네가 진짜 잘하는 집을 안 가봐서 그래</p>
+            <div class="btn-bx">
+              <AppButton
+                to="/restaurants"
+                color="black"
+                shape="round"
+                arrow>
+                잘하는 집 보러가기
+              </AppButton>
+              <AppButton
+                color="black"
+                variant="outline"
+                shape="round"
+                arrow
+                @click="goToRegister">
+                잘하는 집 알려주기
+              </AppButton>
+            </div>
           </div>
         </div>
       </div>
